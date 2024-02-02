d057472b2fc5519ba3c18226a0583933ad503057
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-02 14:20:26 -0600

    added changes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const calculateLove= () => {
     let loveScore= Math.random() * 100;
     loveScore= Math.floor(loveScore) + 1;
     console.log(loveScore);
+    // alert("You and your match are " + loveScore + "% compatible.");
+    alert(`You and your match are ${loveScore}% compatible.`);
 }
 
 
